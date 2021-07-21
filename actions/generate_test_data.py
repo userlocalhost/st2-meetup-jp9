84ec943df019ffa61783089d00e9288ec895ff5c
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2021-07-21 22:34:23 +0900

    Update workflow of passing_data to be able to deal large number of data

--- actions/generate_test_data.py
+++ actions/generate_test_data.py
@@ -2,7 +2,7 @@ from st2common.runners.base_action import Action
 
 
 class GenerateTestData(Action):
-    PAYLOAD_DATA = 'A' * (1 << 10)
+    PAYLOAD_DATA = 'A' * (1 << 5)
 
     def run(self, count):
         return [{
